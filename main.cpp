ccff056db98abc60b2595d241f52de14b2095966
Zdenek <zd.brabec@gmail.com>
2016-12-25 20:22:13 +0100

    word len

--- main.cpp
+++ main.cpp
@@ -24,6 +24,7 @@ struct Dictionary {
 void read_dictionaries(int dictionaries_count, Dictionary *dictionaries, char *input);
 void cleanup(int dictionaries_count, Dictionary *dictionaries);
 void find_occurrences(Word word, char *input, int len);
+bool is_alphabet(int ch);
 
 int main() {
 
@@ -62,17 +63,19 @@ void read_dictionaries(int dictionaries_count, Dictionary *dictionaries, char *i
 
         int words_count;
         std::cin >> words_count;
+        int ch = getchar();
+        if(is_alphabet(ch)) {
+            ungetc(ch, stdin);
+        }
 
         dictionaries[i].words = new Word[words_count];
         dictionaries[i].words_count = words_count;
         for(int j=0;j<words_count;j++) {
             dictionaries[i].words[j].word = new char[WORD_SIZE];
-            scanf("%s",dictionaries[i].words[j].word);
 
-            while(dictionaries[i].words[j].word[dictionaries[i].words[j].length] > 96 && dictionaries[i].words[j].word[dictionaries[i].words[j].length] < 123) {
+            while(is_alphabet(dictionaries[i].words[j].word[dictionaries[i].words[j].length]=getchar())) {
                 dictionaries[i].words[j].length++;
             }
-            std::cout << dictionaries[i].words[j].length << std::endl;
 
             find_occurrences(dictionaries[i].words[j], input, MAX_INPUT_SIZE);
         }
@@ -89,4 +92,8 @@ void find_occurrences(Word word, char *input, int len) {
         i++;
     }
 
+}
+
+bool is_alphabet(int ch) {
+    return ch > 96 && ch < 123;
 }
