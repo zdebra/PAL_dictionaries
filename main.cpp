5b6b1d833e7a0d74061a4b14a4637c9d7d6295c6
Zdenek <zd.brabec@gmail.com>
2016-12-26 19:51:54 +0100

    occurrencies

--- main.cpp
+++ main.cpp
@@ -5,13 +5,13 @@ const int WORD_SIZE = 20;
 
 struct Occurrence {
     int position = 0;
-    Occurrence *next;
+    Occurrence *next = NULL;
 };
 
 struct Word {
     char *word;
     int length = 0;
-    Occurrence *occurrence;
+    Occurrence *occurrence = NULL;
 };
 
 struct Dictionary {
@@ -23,8 +23,9 @@ struct Dictionary {
 
 void read_dictionaries(int dictionaries_count, Dictionary *dictionaries, char *input);
 void cleanup(int dictionaries_count, Dictionary *dictionaries);
-void find_occurrences(Word word, char *input, int len);
+void find_occurrences(Word &word, char *input, int len);
 bool is_alphabet(int ch);
+bool is_same(Word &word, int pos, char *input);
 
 int main() {
 
@@ -73,25 +74,56 @@ void read_dictionaries(int dictionaries_count, Dictionary *dictionaries, char *i
         for(int j=0;j<words_count;j++) {
             dictionaries[i].words[j].word = new char[WORD_SIZE];
 
-            while(is_alphabet(dictionaries[i].words[j].word[dictionaries[i].words[j].length]=getchar())) {
+            while(is_alphabet(dictionaries[i].words[j].word[dictionaries[i].words[j].length]= (char) getchar())) {
                 dictionaries[i].words[j].length++;
             }
 
             find_occurrences(dictionaries[i].words[j], input, MAX_INPUT_SIZE);
+
+           /* std::cout << dictionaries[i].words[j].word << ", " << dictionaries[i].words[j].length << ", ";
+            Word word = dictionaries[i].words[j];
+            Occurrence *cur = dictionaries[i].words[j].occurrence;
+            while(cur != NULL) {
+                std::cout << cur->position << ", ";
+                cur = cur->next;
+            }
+            std::cout << std::endl;
+            */
         }
 
 
     }
 }
 
-void find_occurrences(Word word, char *input, int len) {
+void find_occurrences(Word &word, char *input, int len) {
 
-    int i = 0;
-    while(input[i] != '\n' && i < len) {
-
+    Occurrence *cur = NULL;
+    int i = word.length-1;
+    while(is_alphabet(input[i]) && i < len) {
+        if(is_same(word, i, input)) {
+            // set occurrence
+            if(cur == NULL) {
+                cur = new Occurrence;
+                cur->position = i+1-word.length;
+                word.occurrence = cur;
+            } else {
+                cur->next = new Occurrence;
+                cur = cur->next;
+                cur->position = i+1-word.length;
+            }
+        }
         i++;
     }
 
+}
+
+bool is_same(Word &word, int pos, char *input) {
+    for(int j= word.length - 1; j >= 0; j--) {
+        if(word.word[j] != input[pos-word.length+1+j]) {
+            return false;
+        }
+    }
+    return true;
 }
 
 bool is_alphabet(int ch) {
